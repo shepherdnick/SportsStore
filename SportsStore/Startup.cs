9bb683f7cbc6e595a14b8e1f97ca704c7743877e
Nick Shepherd <shepherdnick@hotmail.com>
2018-10-06 15:52:59 +0100

    Added pagination and better routing.

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -33,6 +33,10 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "pagination",
+                    template: "Products/Page{productPage}",
+                    defaults: new { Controller = "Product", action = "List" });
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
             });
