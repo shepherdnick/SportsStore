b1dc7790faf52a83e5517cc2704135d8ec12a03f
Nick Shepherd <shepherdnick@hotmail.com>
2018-10-11 21:41:52 +0100

    Created a cart and made the ability for items to be added to the cart.

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -22,6 +22,8 @@ namespace SportsStore
                     Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +32,7 @@ namespace SportsStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
