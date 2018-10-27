94974fcc36ecbd5e3412e29c61c17768d195750c
Nick Shepherd <shepherdnick@hotmail.com>
2018-10-27 15:10:13 +0100

    Added an administration layer to the application so that products can be added, edited and deleted.

--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -13,5 +13,38 @@ namespace SportsStore.Models
         }
 
         public IQueryable<Product> Products => context.Products;
+
+        public Product DeleteProduct(int productId)
+        {
+            Product dbEntry = context.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if(dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+
+            return dbEntry;
+        }
+
+        public void SaveProduct(Product product)
+        {
+            if(product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.Products.FirstOrDefault(p => p.ProductID == product.ProductID);
+                if(dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+        }
     }
 }
--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace SportsStore.Models
 {
     public class Product
@@ -9,24 +11,29 @@ namespace SportsStore.Models
             set;
         }
 
+        [Required(ErrorMessage = "Please enter a valid name.")]
         public string Name
         {
             get;
             set;
         }
 
+        [Required(ErrorMessage = "Please enter a valid description.")]
         public string Description
         {
             get;
             set;
         }
 
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive value.")]
         public decimal Price
         {
             get;
             set;
         }
 
+        [Required(ErrorMessage = "Please specify a category.")]
         public string Category
         {
             get;
